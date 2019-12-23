0599bbd01560fa009745befaf190bb5213de0cdb
Jake Nielsen <jnielsen@dialpad.com>
2019-12-23 14:42:28 -0800

    SMS api bugfix

--- dialpad/resources/sms.py
+++ dialpad/resources/sms.py
@@ -3,13 +3,13 @@ from .resource import DialpadResource
 class SMSResource(DialpadResource):
   _resource_path = ['sms']
 
-  def post(self, user_id, to_numbers, text, target_id=None, target_type=None):
+  def post(self, user_id, to_numbers, text, sender_group_id=None, sender_group_type=None):
     return self.request(method='POST', data={
       'text': text,
       'user_id': user_id,
       'to_numbers': to_numbers,
-      'target_id': target_id,
-      'target_type': target_type,
+      'sender_group_id': sender_group_id,
+      'sender_group_type': sender_group_type,
     })
 
   def list(self, state='active', limit=25):
