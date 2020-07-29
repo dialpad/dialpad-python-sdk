6aeacb1f177207b42c956bb59e57faffb987cd6d
Jake Nielsen <jnielsen@dialpad.com>
2020-07-28 18:29:03 -0700

    Pre-publication prep

--- setup.py
+++ setup.py
@@ -7,11 +7,15 @@ with open('README.md') as f:
 
 setup(
   name='python-dialpad',
-  version='0.1.0',
+  version='1.0.0',
   description='A python wrapper for the Dialpad REST API',
   long_description=readme,
+  long_description_content_type="text/markdown",
   author='Jake Nielsen',
   author_email='jnielsen@dialpad.com',
+  license='MIT',
   url='https://github.com/dialpad/dialpad-python-sdk',
+  install_requires=['cached-property', 'certifi', 'chardet', 'idna', 'requests', 'urllib3'],
+  include_package_data=True,
   packages=find_packages()
 )
