7a448ff9402c9bffd3809b311444619ed9bab2c1
Jake Nielsen <jnielsen@dialpad.com>
2021-08-20 11:18:35 -0700

    Release version 2.0.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 setup(
   name='python-dialpad',
-  version='1.7.0',
+  version='2.0.0',
   description='A python wrapper for the Dialpad REST API',
   long_description=readme(),
   long_description_content_type="text/markdown",
