3d5eea6635bac1eceed2929e36370dcc207a1dca
Alex Unger <alex@helloheyday.com>
2022-02-17 10:55:13 -0800

    updated with review comments

--- dialpad/client.py
+++ dialpad/client.py
@@ -53,10 +53,9 @@ class DialpadClient(object):
       data['cursor'] = response_json['cursor']
       response = self._raw_request(path, method, data, headers)
       response.raise_for_status()
-      if response != []:
-        response_json = response.json()
-        for i in response_json['items']:
-          yield i
+      response_json = response.json() or dict()
+      for i in response_json['items']:
+        yield i
 
   def _raw_request(self, path, method='GET', data=None, headers=None):
     url = self._url(*path)
