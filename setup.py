9ba9e67461c4ff908155950aa1ba5fc92e771df4
Yusuf Ertekin <yusuf.ertekin@dialpad.com>
2023-02-28 23:56:48 +0300

    Release version 2.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 setup(
   name='python-dialpad',
-  version='2.2.0',
+  version='2.2.1',
   description='A python wrapper for the Dialpad REST API',
   long_description=readme(),
   long_description_content_type="text/markdown",
