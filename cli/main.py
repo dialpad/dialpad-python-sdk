abfa7ec968cf690cf6f4ba6614e13c45d6c470c1
Jake Nielsen <jnielsen@dialpad.com>
2025-06-05 12:01:08 -0700

    Adds a simple generate-module cli command

--- cli/main.py
+++ cli/main.py
@@ -1,17 +1,41 @@
+import ast
+from typing import Annotated
+import inquirer
 import os
 import typer
+
+from openapi_core import OpenAPI
 
 REPO_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 SPEC_FILE = os.path.join(REPO_ROOT, 'dialpad_api_spec.json')
 
+from cli.client_gen.resource_modules import resource_path_to_module_def
+
+
 app = typer.Typer()
 
 
-@app.command()
-def hello(name: str):
-  print(f"Hello {name}")
+@app.command('gen-module')
+def generate_resource_module(output_file: Annotated[str, typer.Argument(help="The name of the output file to write the resource module.")]):
+  """Prompts the user to select a resource path, and then generates a Python resource module from the OpenAPI specification."""
+  open_api_spec = OpenAPI.from_file_path(SPEC_FILE)
 
-@app.command()
+  questions = [
+    inquirer.List(
+      'path',
+      message='Select the resource path to convert to a module',
+      choices=(open_api_spec.spec / 'paths').keys(),
+    ),
+  ]
+  answers = inquirer.prompt(questions)
+  if not answers:
+    return typer.echo('No selection made. Exiting.')
+
+  module_def = resource_path_to_module_def(open_api_spec.spec / 'paths' / answers['path'])
+  with open(output_file, 'w') as f:
+    f.write(ast.unparse(ast.fix_missing_locations(module_def)))
+
+@app.command('goodbye')
 def goodbye(name: str):
   print(f"Goodbye {name}")
 
