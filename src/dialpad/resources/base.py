8bb5706aa0a842ea9c66a26d3882dbe455c4ba86
Jake Nielsen <jnielsen@dialpad.com>
2025-06-11 12:17:39 -0700

    Bug fix

--- src/dialpad/resources/base.py
+++ src/dialpad/resources/base.py
@@ -2,12 +2,10 @@ from typing import Optional, Iterator
 
 
 class DialpadResource(object):
-  _resource_path = None
-
   def __init__(self, client):
     self._client = client
 
-  def request(
+  def _request(
     self,
     method: str = 'GET',
     sub_path: Optional[str] = None,
@@ -15,18 +13,11 @@ class DialpadResource(object):
     body: Optional[dict] = None,
     headers: Optional[dict] = None,
   ) -> dict:
-    if self._resource_path is None:
-      raise NotImplementedError('DialpadResource subclasses must define a _resource_path property')
-
-    _path = self._resource_path
-    if sub_path:
-      _path = f'{_path}/{sub_path}'
-
     return self._client.request(
-      method=method, sub_path=_path, params=params, body=body, headers=headers
+      method=method, sub_path=sub_path, params=params, body=body, headers=headers
     )
 
-  def iter_request(
+  def _iter_request(
     self,
     method: str = 'GET',
     sub_path: Optional[str] = None,
@@ -34,13 +25,6 @@ class DialpadResource(object):
     body: Optional[dict] = None,
     headers: Optional[dict] = None,
   ) -> Iterator[dict]:
-    if self._resource_path is None:
-      raise NotImplementedError('DialpadResource subclasses must define a _resource_path property')
-
-    _path = self._resource_path
-    if sub_path:
-      _path = f'{_path}/{sub_path}'
-
     return self._client.iter_request(
-      method=method, sub_path=_path, params=params, body=body, headers=headers
+      method=method, sub_path=sub_path, params=params, body=body, headers=headers
     )
